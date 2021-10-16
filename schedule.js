d634d2b2a966c1741588316e92e4103acabf2415
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-10-16 14:57:32 -0600

    updated text area and save buttons created js file with pseudo code

---
+++ schedule.js
@@ -0,0 +1,9 @@
+// get current date and time from moment 
+
+//create time blocks for the day via moment 
+
+//click events to buttons 
+
+
+//color code time blocks based on when a task is over due, due in the future or current 
+
