b4f18a057ac01ed8fa9b08624c208739bdcd35a1
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-10-18 16:55:27 -0600

    moment date added and time blockls formatted

--- schedule.js
+++ schedule.js
@@ -1,8 +1,19 @@
 // get current date and time from moment 
+var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").append(currentDay);
+
 
 //create time blocks for the day via moment 
+$("#9am")
+
+
+
+
+
 
 //click events to buttons 
+//function to save to local storage 
+
 
 
 //color code time blocks based on when a task is over due, due in the future or current 
