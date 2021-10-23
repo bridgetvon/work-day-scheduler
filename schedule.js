9a284f5680eb910cc6746eb170b7c4a11d2477bd
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-10-23 11:59:11 -0600

    five day forcast and styling

--- schedule.js
+++ schedule.js
@@ -3,18 +3,26 @@ var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(currentDay);
 
 
-//create time blocks for the day via moment 
-$("#9am")
+//color code time blocks based on when a task is over due, due in the future or current 
 
-
+var currentTime= (new Date()).getHours();
+function colorBlock() {
+var timeOfDay = getHours();
+switch(true) {
+    case (n > 9) :
+        
+}
 
 
 
 
 //click events to buttons 
 //function to save to local storage 
+function saveTask () {
+    $("saveBtn").on("click", save);
+    console.log("button clicked");
+}
 
 
 
-//color code time blocks based on when a task is over due, due in the future or current 
 
